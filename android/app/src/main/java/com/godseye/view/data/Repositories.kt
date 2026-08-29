898a7a3d89ef3cb4cb7101fc359bb81d07ee7150
mexicanbr0auth <gabriel@example.com>
2026-08-29 18:18:36 -0300

    fix: import Context em Repositories.kt

--- android/app/src/main/java/com/godseye/view/data/Repositories.kt
+++ android/app/src/main/java/com/godseye/view/data/Repositories.kt
@@ -6,6 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import android.content.Context
 import com.google.android.gms.maps.model.LatLng
 import java.util.concurrent.TimeUnit
 
